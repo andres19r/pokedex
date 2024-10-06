bd2fadc6a7a3881a4a25366a378fdc36b7ca7c50
Andres Rivero <andres19rivero@gmail.com>
2024-10-05 22:15:13 -0400

    feat(seed): implement initial functionality to execute method

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [SeedController],
   providers: [SeedService],
 })
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,24 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  execute() {
-    return 'Seed executed';
+  constructor(private readonly httpService: HttpService) {}
+
+  async execute() {
+    const { data } = await firstValueFrom(
+      this.httpService.get<PokeResponse>(
+        'https://pokeapi.co/api/v2/pokemon?limit=10',
+      ),
+    );
+    const newPokemons = data.results.map(({ name, url }) => {
+      const segments = url.split('/');
+      const no: number = +segments.at(-2);
+      return { name, no };
+    });
+    console.log(newPokemons);
+    return data.results[0];
   }
 }
