d7cbbd935680e6705b1ab9567ee697e833a94ae1
Andres Rivero <andres19rivero@gmail.com>
2024-10-06 00:33:25 -0400

    feat: implement pagination to get all pokemon endpoint

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -9,6 +9,7 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -26,9 +27,14 @@ export class PokemonService {
     }
   }
 
-  async findAll() {
-    const pokemonList = await this.pokemonModel.find();
-    return pokemonList;
+  async findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+    return this.pokemonModel.aggregate([
+      { $skip: offset },
+      { $limit: limit },
+      { $sort: { no: 1 } },
+      { $project: { __v: 0 } },
+    ]);
   }
 
   async findOne(term: string) {
